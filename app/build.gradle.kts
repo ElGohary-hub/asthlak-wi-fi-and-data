689e73b469dbe4ed3fa2dc9d125bae9fa5c9b52e
ElGohary-hub <adel16384y@gmail.com>
2026-06-22 19:52:33 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,20 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+// السطر السحري ده هو اللي هيوحد الإصدار ويمنع خطأ التعارض بتاع الـ 1.8
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
@@ -29,7 +43,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     
-    // Jetpack Compose BOM (أحدث إصدار مستقر)
+    // Jetpack Compose BOM
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
